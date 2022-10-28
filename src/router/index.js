387f06d407cd42369e03d3696027694421ae6931
lapinRagnar <lapinragnar@gmail.com>
2022-10-28 10:08:15 +0200

    2 - ajout pages Home, Login, Signup ajout router : Login et Signup

--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,26 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import Home from '@/views/Home.vue'
+import Signup from '@/views/Signup.vue'
+import Login from '@/views/Login.vue'
+
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: HomeView
+      name: 'Home',
+      component: Home
+    },
+    {
+      path: '/signup',
+      name: 'Signup',
+      component: Signup
+    },
+    {
+      path: '/login',
+      name: 'Login',
+      component: Login
     },
     {
       path: '/about',

971548675fb23ec3d0148ee85ed24daa7deca4d8
lapinRagnar <lapinragnar@gmail.com>
2022-10-30 12:52:08 +0100

    login () actions

--- src/store/index.js
+++ src/store/index.js
@@ -3,6 +3,7 @@ import { createStore } from "vuex"
 import { auth } from '@/firebase/config'
 import {
   createUserWithEmailAndPassword , 
+  signInWithEmailAndPassword,
 
 } from 'firebase/auth'
 
@@ -19,15 +20,36 @@ const store = createStore({
   },
 
   actions: {
+    
     async signup(context, { email, password}) {
+
       console.log('---------signup action---------')
 
-      const res = await createUserWithEmailAndPassword(auth, email, password) 
+      const res = await createUserWithEmailAndPassword(auth, email, password)
+      console.log('resultat signup', res) 
+
       if (res) {
         context.commit('setUser', res.user)
       } else {
         throw new Error('could not complete signup ')
       }
+
+    },
+
+    async login(context, { email, password }) {
+
+      console.log('---------login action---------')
+
+      const res = await signInWithEmailAndPassword(auth, email, password) 
+
+      console.log('resultat login', res) 
+
+      if (res) {
+        context.commit('setUser', res.user)
+      } else {
+        throw new Error('could not complete login ')
+      }
+
     }
   }
 
